18185d033897a2865da670f9901a1b405bbe885b
daju233 <2212509690@qq.com>
2024-12-15 01:41:19 +0800

    assign5 finish

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -1,31 +1,23 @@
 #include "user.h"
-
+#include <set>
 /**
  * Creates a new User with the given name and no friends.
  */
-User::User(const std::string& name)
-  : _name(name)
-  , _friends(nullptr)
-  , _size(0)
-  , _capacity(0)
-{
-}
+User::User(const std::string& name) : _name(name), _friends(nullptr), _size(0), _capacity(0) {}
 
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
-{
+void User::add_friend(const std::string& name) {
   if (_size == _capacity) {
     _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
+    std::string* others = new std::string[_capacity];
     for (size_t i = 0; i < _size; ++i) {
-      newFriends[i] = _friends[i];
+      others[i] = _friends[i];
     }
     delete[] _friends;
-    _friends = newFriends;
+    _friends = others;
   }
 
   _friends[_size++] = name;
@@ -34,32 +26,64 @@ User::add_friend(const std::string& name)
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
-{
-  return _name;
-}
+std::string User::get_name() const { return _name; }
 
 /**
  * Returns the number of friends this User has.
  */
-size_t
-User::size() const
-{
-  return _size;
-}
+size_t User::size() const { return _size; }
 
 /**
  * Sets the friend at the given index to the given name.
  * @param index The index of the friend to set.
  * @param name The name to set the friend to.
  */
-void User::set_friend(size_t index, const std::string& name)
-{
-  _friends[index] = name;
-}
+void User::set_friend(size_t index, const std::string& name) { _friends[index] = name; }
 
-/** 
+/**
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+
+User& operator+=(User& user,User& other){
+  user.add_friend(other.get_name());
+  other.add_friend(user.get_name());
+  return user;
+}
+
+bool operator<(const User& user,const User& other){
+  std::set<std::string> set;
+  set.insert(user.get_name());
+  set.insert(other.get_name());
+   // 获取两个元素的迭代器
+   auto it1 = set.find(user.get_name());
+   auto it2 = set.find(other.get_name());
+  return *it1<*it2;
+}
+
+std::ostream& operator<<(std::ostream& output, const User& user) {
+  output << "User(name=" << user.get_name() << ", friends=[";
+  // 不要起一个varable = user.size()的变量，这样写非常容易越界ub
+  // 我是傻逼，所以不要这么写
+  // 可以输出每一个元素和后面的逗号，检查是否是最后一个元素
+  for (size_t i = 0; i < user.size(); i++) {
+    output << user._friends[i];
+    if (i != user.size() - 1) {
+      output << ", ";
+    }
+  }
+  // 也可以这样写
+  /*
+    检查是否有朋友列表
+    if (user.size() > 0) {
+    输出第一个朋友，不带前缀的逗号
+    output << user._friends[0];
+
+    如果有超过一个朋友，输出剩余的朋友并用逗号分隔
+    for (size_t i = 1; i < user.size(); ++i) {
+    output << ", " << user._friends[i];
+    }
+  */
+  output << "])";
+  return output;
+}
--- assign5/user.h
+++ assign5/user.h
@@ -6,8 +6,7 @@
 #include <iostream>
 #include <string>
 
-class User
-{
+class User {
 public:
   User(const std::string& name);
   void add_friend(const std::string& name);
@@ -15,10 +14,49 @@ public:
   size_t size() const;
   void set_friend(size_t index, const std::string& name);
 
-  /** 
+  /**
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  friend User& operator+=(User& user,User& other);
+  friend bool operator<(const User& user,const User& other);
+  friend std::ostream& operator<<(std::ostream& output, const User& user);
+
+  // Implement a destructor for the User class. To do so, implement the User::~User() SMF.
+  ~User() { delete[] this->_friends; };
+
+  // Make the User class copy constructible. To do so, implement the User::User(const User& user)
+  // SMF.
+  User(const User& user) {
+    this->_capacity = user._capacity;
+    this->_size = user._size;
+    this->_name = user._name;
+    this->_friends = new std::string[user._size];
+    for (size_t i = 0; i < user._size; ++i) {
+      this->_friends[i] = user._friends[i];
+    }
+  };
+
+  // Make the User class copy assignable. To do so, implement the User& User::operator=(const User&
+  // user) SMF.
+  User& operator=(const User& user) {
+    this->_capacity = user._capacity;
+    this->_size = user._size;
+    this->_name = user._name;
+    delete[] this->_friends;
+    this->_friends = new std::string[user._size];
+    for (size_t i = 0; i < user._size; ++i) {
+      this->_friends[i] = user._friends[i];
+    }
+    return *this;
+  };
+
+  // Prevent the User class from being move constructed. To do so, delete the User::User(User&&
+  // user) SMF. Prevent the User class from being move assigned. To do so, delete the User&
+  // User::operator=(User&& user) SMF.
+  User(User&& user) = delete;
+  User& operator=(User&& user) = delete;
+
 
 private:
   std::string _name;
